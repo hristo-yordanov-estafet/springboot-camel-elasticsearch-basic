2404e891bf90a550c86f5cfb5e4025fe5d46b3f2
Hristo Yordanov <hristo.yordanov@estafet.com>
2018-01-15 17:08:06 +0200

    check file exists test

--- src/main/java/example/route/ImportProductsCvsRoute.java
+++ src/main/java/example/route/ImportProductsCvsRoute.java
@@ -1,9 +1,21 @@
 package example.route;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
+import org.apache.camel.RouteNode;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
+import org.apache.camel.management.InstrumentationProcessor;
+import org.apache.camel.processor.interceptor.Tracer;
+import org.apache.camel.spi.TracedRouteNodes;
 import org.elasticsearch.client.transport.NoNodeAvailableException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import example.model.Product;
@@ -11,14 +23,30 @@ import example.model.Product;
 @Component
 public class ImportProductsCvsRoute extends RouteBuilder {
 	
+	@Autowired
+	CamelContext camelContext;
+	
 	@Override
     public void configure() throws Exception {
-		System.out.println("***************** start ImportProductsCvsRoute");
+
+		getContext().setTracing(true);
+
+        
         BindyCsvDataFormat csv = new BindyCsvDataFormat(Product.class);
         
-        from("file://src/main/resources?fileName=products.csv&noop=true").id(("import-products-cvs-route"))
+        File directory = new File("./");
+        System.out.println("PATH-------------------" + directory.getAbsolutePath());
+        
+        File f = new File("src/main/resources/products.csv");
+        if(f.exists() && !f.isDirectory()) { 
+        	System.out.println("***************** file exists");
+        } else {
+        	System.out.println("***************** file not exists");
+        }
+        
+        from("file://src/main/resources?fileName=products.csv&noop=true&directoryMustExist=true&fileExist=Ignore").id(("import-products-cvs-route"))
             //.onException(NoNodeAvailableException.class).maximumRedeliveries(2).to("direct://error").handled(true).end()
-            .onException(Exception.class).to("log:error? showCaughtException=true& showStackTrace=true")
+            //.onException(Exception.class).log("************************************************************fole exception").end()
             .log("Records received : ${body}")
             .unmarshal(csv)
             .split(body())
@@ -29,5 +57,7 @@ public class ImportProductsCvsRoute extends RouteBuilder {
 //        .log("No node Elasticsearch server is available");
 
     }
+	
+
 
 }
